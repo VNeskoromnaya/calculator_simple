86ca693474ade995efc70d995689937dfeb5b2a1
VNeskoromnaya <neskoromnaya.v@gmail.com>
2022-01-26 19:37:23 +0200

    js

---
+++ assets/main.js
@@ -0,0 +1,35 @@
+//debugger
+const num1Input = document.getElementById('num1'),
+      num2Input = document.getElementById('num2'),
+      totalIntup = document.getElementById('total');
+
+let total;
+
+const addition = () => {
+    let total = Number(num1Input.value) + Number(num2Input.value);
+    document.getElementById('num1').value = " ";
+    document.getElementById('num2').value = " ";
+    totalIntup.value = total;
+}
+
+const subtraction = () => {
+    let total = Number(num1Input.value) - Number(num2Input.value);
+    document.getElementById('num1').value = " ";
+    document.getElementById('num2').value = " ";
+    totalIntup.value = total;
+}
+
+const multiplication = () => {
+    let total = Number(num1Input.value) * Number(num2Input.value);
+    document.getElementById('num1').value = " ";
+    document.getElementById('num2').value = " ";
+    totalIntup.value = total;
+}
+
+const division = () => {
+    let total = Number(num1Input.value) / Number(num2Input.value);
+    document.getElementById('num1').value = " ";
+    document.getElementById('num2').value = " ";
+    totalIntup.value = total;
+    console.log(totalIntup.value);
+}
