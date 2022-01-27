42662659618ba492878421d0cff6f17bde4eb5db
VNeskoromnaya <neskoromnaya.v@gmail.com>
2022-01-27 08:44:06 +0200

    update js

--- assets/main.js
+++ assets/main.js
@@ -7,29 +7,29 @@ let total;
 
 const addition = () => {
     let total = Number(num1Input.value) + Number(num2Input.value);
-    document.getElementById('num1').value = " ";
-    document.getElementById('num2').value = " ";
+    document.getElementById('num1').value = "";
+    document.getElementById('num2').value = "";
     totalIntup.value = total;
 }
 
 const subtraction = () => {
     let total = Number(num1Input.value) - Number(num2Input.value);
-    document.getElementById('num1').value = " ";
-    document.getElementById('num2').value = " ";
+    document.getElementById('num1').value = "";
+    document.getElementById('num2').value = "";
     totalIntup.value = total;
 }
 
 const multiplication = () => {
     let total = Number(num1Input.value) * Number(num2Input.value);
-    document.getElementById('num1').value = " ";
-    document.getElementById('num2').value = " ";
+    document.getElementById('num1').value = "";
+    document.getElementById('num2').value = "";
     totalIntup.value = total;
 }
 
 const division = () => {
     let total = Number(num1Input.value) / Number(num2Input.value);
-    document.getElementById('num1').value = " ";
-    document.getElementById('num2').value = " ";
+    document.getElementById('num1').value = "";
+    document.getElementById('num2').value = "";
     totalIntup.value = total;
     console.log(totalIntup.value);
 }
