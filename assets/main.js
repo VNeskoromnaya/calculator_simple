2d1a5c6fba60152838dcef6fea6bae83fea091ca
VNeskoromnaya <neskoromnaya.v@gmail.com>
2022-10-17 12:08:12 +0300

    update + creating class Calculator

--- assets/main.js
+++ assets/main.js
@@ -1,44 +1,59 @@
-//debugger
-const num1Input = document.getElementById('num1'),
-      num2Input = document.getElementById('num2'),
-      totalIntup = document.getElementById('total');
+/*
+Создайте класс Калькулятор, который будет содержать 4 статических метода - Сложение, Умножение, Вычитание и Деление. Доработайте свою страничку с калькулятором, чтобы в ней использовались методы этого класса.
+*/
+
+const num1Input = document.getElementById("num1");
+const num2Input = document.getElementById("num2");
+const totalIntup = document.getElementById("total");
+const btnAddition = document.querySelector("#btnAddition");
+const btnSubtraction = document.querySelector("#btnSubtraction");
+const btnMultiplication = document.querySelector("#btnMultiplication");
+const btnDivision = document.querySelector("#btnDivision");
 
 let total;
 
-const addition = () => {
-    let total = Number(num1Input.value) + Number(num2Input.value);
-    document.getElementById('total_error').style.display = 'none';
+class Calculator {
+  static addition = () => {
+    total = Number(num1Input.value) + Number(num2Input.value);
+    document.getElementById("total_error").style.display = "none";
     totalIntup.value = total;
-    document.getElementById('num1').value = "";
-    document.getElementById('num2').value = "";
-}
+    document.getElementById("num1").value = "";
+    document.getElementById("num2").value = "";
+  };
 
-const subtraction = () => {
-    let total = Number(num1Input.value) - Number(num2Input.value);
-    document.getElementById('total_error').style.display = 'none';
+  static subtraction = () => {
+    total = Number(num1Input.value) - Number(num2Input.value);
+    document.getElementById("total_error").style.display = "none";
     totalIntup.value = total;
-    document.getElementById('num1').value = "";
-    document.getElementById('num2').value = "";
-}
+    document.getElementById("num1").value = "";
+    document.getElementById("num2").value = "";
+  };
 
-const multiplication = () => {
-    let total = Number(num1Input.value) * Number(num2Input.value);
-    document.getElementById('total_error').style.display = 'none';
+  static multiplication = () => {
+    total = Number(num1Input.value) * Number(num2Input.value);
+    document.getElementById("total_error").style.display = "none";
     totalIntup.value = total;
-    document.getElementById('num1').value = "";
-    document.getElementById('num2').value = "";
-}
+    document.getElementById("num1").value = "";
+    document.getElementById("num2").value = "";
+  };
 
-const division = () => {
-    let total = Number(num1Input.value) / Number(num2Input.value);
-    if(Number(num2Input.value) == '0') {
-    totalIntup.value = 'Error!';
-    document.getElementById('total_error').style.display = 'block';
+  static division = () => {
+    total = Number(num1Input.value) / Number(num2Input.value);
+    if (Number(num2Input.value) == "0") {
+      totalIntup.value = "Error!";
+      console.log("WOW, я работаю");
+      document.getElementById("total_error").style.display = "block";
+    } else {
+      totalIntup.value = total;
+      console.log("я работаю");
+      document.getElementById("total_error").style.display = "none";
     }
-    else {
-        totalIntup.value = total;
-        document.getElementById('total_error').style.display = 'none';
-    }
-    document.getElementById('num1').value = "";
-    document.getElementById('num2').value = "";
+    document.getElementById("num1").value = "";
+    document.getElementById("num2").value = "";
+  };
 }
+
+btnAddition.addEventListener("click", Calculator.addition);
+btnSubtraction.addEventListener("click", Calculator.subtraction);
+btnMultiplication.addEventListener("click", Calculator.multiplication);
+btnDivision.addEventListener("click", Calculator.division);
